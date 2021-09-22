379fe4fa8248ab0721c79bbca442be0164ab0e36
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-22 20:38:53 +0200

    Improved the app on logic

--- app/src/main/java/com/afrosoft/yourweighton/MainActivity.kt
+++ app/src/main/java/com/afrosoft/yourweighton/MainActivity.kt
@@ -16,32 +16,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var weight = enterWeightId.text
 
         showWeightButtonId.setOnClickListener {
-            var result = calculateWeight(weight.toString().toDouble())
-            resultTextView.text = "You weigh "+result.toString()+" pounds on mars"
+//            var result = calculateWeight(weight.toString().toDouble())
+//            resultTextView.text = "You weigh "+result.toString()+" pounds on mars"
         }
         marscheckBox.setOnClickListener(this)//Register our clickListener
         venuscheckBox.setOnClickListener(this)
         jupitercheckBox.setOnClickListener(this)
 
     }
-    fun calculateWeight(userWeight: Double): Double{
-        return userWeight * marsGravity
-    }
-
     override fun onClick(v: View?) {
         v as CheckBox
         var isChecked: Boolean = v.isChecked
+        var weight = enterWeightId.text
 
         when(v.id){
             R.id.marscheckBox -> if(isChecked){
-                Log.d("Setup Mars","Mars")}
+                calculateWeight(weight.toString().toDouble(),v)}
             R.id.venuscheckBox -> if(isChecked){
-                Log.d("Setup Venus", "Venus")}
+                calculateWeight(weight.toString().toDouble(),v)}
             R.id.jupitercheckBox -> if(isChecked){
-                Log.d("Setup Jupiter","Jupiter")}
+                calculateWeight(weight.toString().toDouble(),v)}
         }
+    }
+    fun calculateWeight(userWeight: Double, checkBox: CheckBox){
+        var result: Double?
+        when(checkBox.id){
+            R.id.marscheckBox -> result = userWeight * marsGravity
+            R.id.venuscheckBox -> result = userWeight * venusGravity
+            R.id.jupitercheckBox -> result = userWeight * jupiterGravity
+            else -> result = userWeight * marsGravity
+        }
+        resultTextView.text = "Weight is $result "
     }
 }
